186cab288c00fa805ba4f5b0ec65b220bb399d2d
LinuzLimz <56245918+LinuzLimz@users.noreply.github.com>
2021-12-02 13:58:36 +0800

    searchbar

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome!"
+    return render_template('index.html')
 
 @app.route('/how are you')
 def hello():
