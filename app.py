942814f3e593f61bba59a54e9c7fd656640077da
LinuzLimz <56245918+LinuzLimz@users.noreply.github.com>
2021-12-02 17:54:27 +0800

    update

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
-@app.route('/how are you')
-def hello():
-    return 'I am good, how about you?'
+@app.route('/search')
+def search():
+    return render_template('search.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
